aad3e532d3281b06604ebeb4f535ea173ecf1667
John Dosselman <johndosselman@gmail.com>
2022-05-19 00:20:47 -0500

    step-7

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,6 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
+import 'package:mynotes/views/verify_email_view.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
 
@@ -54,22 +58,30 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                devtools.log(userCredential.toString());
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'invalid-email') {
-                  devtools.log('Invalid email');
+                  showErrorDialog(context, 'Invalid email address');
                 } else if (e.code == 'weak-password') {
-                  devtools.log('weak password');
+                  showErrorDialog(context, 'Password too weak');
                 } else if (e.code == 'email-already-in-use') {
-                  devtools.log('Email is already in use');
+                  showErrorDialog(context, 'Email already in use');
                 } else if (e.code == 'network-request-failed') {
-                  devtools.log('Network error. Check connection');
+                  showErrorDialog(context, 'Network error. Check connection');
                 } else {
-                  devtools.log(e.code);
+                  showErrorDialog(
+                    context,
+                    e.code,
+                  );
                 }
+              } catch (e) {
+                showErrorDialog(context, e.toString());
               }
             },
             child: const Text('Register'),
@@ -77,7 +89,7 @@ class _RegisterViewState extends State<RegisterView> {
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamedAndRemoveUntil(
-                '/login/',
+                loginRoute,
                 (route) => false,
               );
             },
